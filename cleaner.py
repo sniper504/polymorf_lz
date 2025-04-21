d42bb168b62226cc37681bd80b3341f80ceb12c6
sniper504 <abracadabra52@tutamail.com>
2025-04-21 16:41:06 +0300

    Add files via upload

--- cleaner.py
+++ cleaner.py
@@ -13,7 +13,7 @@ class DeleteDuplicates(Cleaner):
     def clean(self):
         return self.data.drop_duplicates()
 
-    def __invert__(self):  # Унарный оператор `~` для удаления дубликатов
+    def __invert__(self):  # ~ для удаления дубликатов
         initial_rows = len(self.data)  # До удаления
         self.data = self.data.drop_duplicates()
         self.removed_rows = initial_rows - len(self.data)  # Сколько строк удалено
@@ -21,7 +21,7 @@ class DeleteDuplicates(Cleaner):
 
 class SplitByOperation(Cleaner):
     def clean(self):
-        operation_types = self.data["Тип операции"].unique()  # Уникальные операции
+        operation_types = self.data["Тип операции"].unique()  
         separated_files = {}
 
         for operation in operation_types:
@@ -32,5 +32,5 @@ class SplitByOperation(Cleaner):
 
         return separated_files
 
-    def __invert__(self):  # `~` теперь отвечает за разделение датасета
+    def __invert__(self):  # ~ для разделение датасета
         return self.clean()
